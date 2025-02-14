c96b12469062a6f017985d81aac7376ced7addee
jhosenthG <jhosenth@gmail.com>
2025-02-14 16:08:43 -0400

    modified:   App.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/ui/components/projectsCard.tsx 	new file:   src/app/ui/screen/UserScreen.tsx 	new file:   src/app/ui/screen/homeUserScreen.tsx 	new file:   src/app/ui/screen/performanceUserScreen.tsx 	new file:   src/app/ui/screen/reportformScreen.tsx 	new file:   src/app/ui/styles/colors.tsx

---
+++ src/app/ui/styles/colors.tsx
@@ -0,0 +1,5 @@
+//Colors for aplication
+export const backgroundColor = "#F1F1F1"; // Cream white
+export const textColor = "#4B4B4B"; // Dark Gray
+export const accentColor = "#FFFFFF"; // White
+export const secundaryColor = "3EEF85"; //Green
